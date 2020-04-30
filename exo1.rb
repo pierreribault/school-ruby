fabdc5dbd51944e451dc53f8eba4e069c674d25c
Pierre Ribault <contact@pierreribault.fr>
2020-04-30 11:19:25 +0200

    :ambulance: Greater than equal instead of greater than

--- exo1.rb
+++ exo1.rb
@@ -4,8 +4,8 @@ firstString = gets.chomp
 puts "Deuxieme chaine"
 secondString = gets.chomp
 
-if(firstString.length > secondString.length * 2)
-  puts "La taille de la premiere chaine est superieur au double de la seconde"
+if(firstString.length >= secondString.length * 2)
+  puts "La taille de la premiere chaine est au moins superieur au double de la seconde"
 else
-  puts "La taille de la premiere chaine n'est pas superieur au double de la seconde"
+  puts "La taille de la premiere chaine n'est pas au moins superieur au double de la seconde"
 end
